1a4c353b15aa3df7d2df4720f7adf0c3ab056bc6
jrboyd <jrboyd@med.uvm.edu>
2017-10-20 11:25:03 -0400

    remove old plotly stuff

--- server.R
+++ server.R
@@ -5,20 +5,6 @@ bed_path = "~/ShinyApps/shiny_peak_data/beds/"
 # server.R definition
 # source('JG_runx_intersect.R')
 source("functions_process_bw.R")
-get_selected_plot_df = function(source = "xy_select"){
-  eventData <- event_data("plotly_selected", source = source)
-  event_data("plotly_selected", source = source)
-  p_df = plotly_data(main_plot)
-  p_df = p_df[order(p_df$plotting_group),]
-  #gene_type hardcoded as level, ugh
-  grp_counts = table(factor(p_df$group))
-  addons = c(0, cumsum(grp_counts)[-length(grp_counts)]) + 1
-  # If NULL dont do anything
-  if(is.null(eventData) == T) return(NULL)
-  idx = addons[eventData$curveNumber + 1] + eventData$pointNumber
-  p_df[idx,]
-}
-
 
 shinyFiles2load = function(shinyF, roots){
   root_path = roots[shinyF$root]
@@ -180,19 +166,15 @@ server <- function(input, output, session){
     xy_plot_dt(xy_val)
   })
   
-  # output$xy_values = renderPlotly({
   output$xy_values = renderPlot({
     plotted_dt = visible_dt()
+    if(is.null(plotted_dt)) return(NULL)
     #isolate because these trigger updates of xy_plot_dt() which already triggers reactivity
     x_variable = isolate(input$x_variable)
     y_variable = isolate(input$y_variable)
     p = ggplot(plotted_dt) + 
       geom_point(aes(x = xval, y = yval, col = plotting_group)) +
       labs(x = x_variable, y = y_variable, title = "Max FE in regions")
-    # ply = ggplotly(p, source = "xy_select") %>% 
-    #   layout(dragmode =  "select")
-    # main_plot <<- ply
-    # ply
     p
   })
 
@@ -488,32 +470,12 @@ server <- function(input, output, session){
     # bw_gr2dt(bw_gr = bw_gr, qgr = fgr, win_size = 50)
   })
   
-  # Coupled event 1
-  output$PlotSelectedProfiles <- renderPlotly({
-    # Get subset based on selection
-    eventData <- event_data("plotly_selected", source = "scatter")
-    # If NULL dont do anything
-    if(is.null(eventData) == T) return(NULL)
-    if(is.null(input$List1) == T) return(NULL)
-    gid = input$List1
-    # gid = plotted_dt[eventData$pointNumber]$gene_id
-    p = ggplot(prof[gene_name %in% gid])#[1:min(8, length(gid))]])
-    if(input$featureInput3 == "tss"){
-      p = p + geom_line(aes(x = x_tss_dist, y = y, color = group))
-    }else{
-      p = p + geom_line(aes(x = x_summit_dist, y = y, color = group))
-    }
-    p = p + facet_grid(gene_name ~ group) + annotate("line", x = c(-2000,2000), y = rep(ym, 2)) + 
-      labs(x = paste("distance to", input$featureInput3), y = "FE") + 
-      theme(strip.text.y = element_text(size = 8, colour = "black", angle = 0))
-    ggplotly(p)
-  })
-  
   output$XY_Selected <- DT::renderDataTable({
     #Add reactivity for selection
     selected_dt()
   })
   
+  #TODO adapt to not use plotly
   output$List1 <- renderUI({
     eventData <- event_data("plotly_selected", source = "scatter")
     # If NULL dont do anything
